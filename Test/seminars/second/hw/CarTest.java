7d45de66374067f315ca8ff1c2d92b0745f71845
Andrew <telepnev.1997@mail.ru>
2023-10-06 21:44:21 +0700

    Initial commit

---
+++ Test/seminars/second/hw/CarTest.java
@@ -0,0 +1,38 @@
+package seminars.second.hw;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CarTest {
+    Car car;
+
+    @BeforeEach
+    public void initObject() {
+        car = new Car("Toyota", "Rav4", 2022);
+    }
+
+    @Test
+    void excpectedBeInstanceOfVehicle() {
+        assertThat(car).isInstanceOf(Vehicle.class);
+    }
+
+    @Test
+    void testDriveSpeedTest() {
+        car.testDrive();
+        assertThat(car.getSpeed()).isEqualTo(60);
+    }
+
+    @Test
+    void parkSpeedTest() {
+        car.testDrive();
+        car.park();
+        assertThat(car.getSpeed()).isEqualTo(0);
+    }
+
+    @Test
+    void getNumWheelsTest() {
+        assertThat(car.getNumWheels()).isEqualTo(4);
+    }
+}
---
+++ Test/seminars/second/hw/MotorcycleTest.java
@@ -0,0 +1,38 @@
+package seminars.second.hw;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MotorcycleTest {
+    Motorcycle motorcycle;
+
+    @BeforeEach
+    public void initObject() {
+        motorcycle = new Motorcycle("Kawasaki", "Ninja", 2022);
+    }
+
+    @Test
+    void excpectedBeInstanceOfVehicle() {
+        assertThat(motorcycle).isInstanceOf(Vehicle.class);
+    }
+
+    @Test
+    void testDriveSpeedTest() {
+        motorcycle.testDrive();
+        assertThat(motorcycle.getSpeed()).isEqualTo(75);
+    }
+
+    @Test
+    void parkSpeedTest() {
+        motorcycle.testDrive();
+        motorcycle.park();
+        assertThat(motorcycle.getSpeed()).isEqualTo(0);
+    }
+
+    @Test
+    void getNumWheelsTest() {
+        assertThat(motorcycle.getNumWheels()).isEqualTo(2);
+    }
+}
